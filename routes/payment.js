d6e3865f81a02c59155b3847c57abc09db03554a
Husen <husenaiah.gandham@kagamierp.com>
2023-01-08 17:01:37 +0530

    payment integration with razorpay

---
+++ routes/payment.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+const express = require("express");
+const Razorpay = require("razorpay");
+
+const router = express.Router();
--- server.js
+++ server.js
@@ -1,50 +1,117 @@
- //import { PrismaClient } from '@prisma/client'
+//import { PrismaClient } from '@prisma/client'
 const { PrismaClient } = require('@prisma/client');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // 
- 
+require("dotenv").config();
+const Razorpay = require("razorpay");
+
+
 const app = express();
- 
+
 app.use(cors())
 // create express app
- 
+
 // setup the server port
 const port = process.env.PORT || 5000;
- 
+
 // parse request data content type application/x-www-form-rulencoded
-app.use(bodyParser.urlencoded({extended: false}));
- 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // parse request data content type application/json
 app.use(bodyParser.json());
- 
+
 // define root route
 
 const prisma = new PrismaClient()
 
 
 
- app.post('/register', async (req, res)=>{
-  console.log("request",req);
-  console.log("request body",req.body);
-  console.log("request body data",req.body.data1);
-  
-  const datainfo= await prisma.employee.create({
-    data: 
-      
-      { name: req.body.name, addresss: req.body.address, order: req.body.order,time: new Date(req.body.date),
-        jobTitle:"hyderabad" }
-    
-  }) 
+app.post('/orders', async (req, res) => {
+  try {
+    const instance = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_SECRET,
+    });
+
+    const options = {
+      amount: 50000, // amount in smallest currency unit
+      currency: "INR",
+      receipt: "receipt_order_74394",
+    };
+
+    const order = await instance.orders.create(options);
+
+    if (!order) return res.status(500).send("Some error occured");
+
+    res.json(order);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+app.post("/success", async (req, res) => {
+  try {
+    // getting the details back from our font-end
+    const {
+      orderCreationId,
+      razorpayPaymentId,
+      razorpayOrderId,
+      razorpaySignature,
+    } = req.body;
+
+    // Creating our own digest
+    // The format should be like this:
+    // digest = hmac_sha256(orderCreationId + "|" + razorpayPaymentId, secret);
+    const shasum = crypto.createHmac("sha256", "w2lBtgmeuDUfnJVp43UpcaiT");
+
+    shasum.update(`${orderCreationId}|${razorpayPaymentId}`);
+
+    const digest = shasum.digest("hex");
+
+    // comaparing our digest with the actual signature
+    if (digest !== razorpaySignature)
+      return res.status(400).json({ msg: "Transaction not legit!" });
+
+    // THE PAYMENT IS LEGIT & VERIFIED
+    // YOU CAN SAVE THE DETAILS IN YOUR DATABASE IF YOU WANT
+
+    res.json({
+      msg: "success",
+      orderId: razorpayOrderId,
+      paymentId: razorpayPaymentId,
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+app.post('/register', async (req, res) => {
+  console.log("request", req);
+  console.log("request body", req.body);
+  console.log("request body data", req.body.data1);
+
+  const datainfo = await prisma.employee.create({
+    data:
+
+    {
+      name: req.body.name, addresss: req.body.address, order: req.body.order, time: new Date(req.body.date),
+      jobTitle: "hyderabad"
+    }
+
+  })
   res.json(datainfo);
 });
 
 
-app.get('/fetchall', async (req, res)=>{
-  console.log("request",req);
-  console.log("request body",req.body);
-  console.log("request body data",req.body.data);
-  
+
+app.get('/fetchall', async (req, res) => {
+  console.log("request", req);
+  console.log("request body", req.body);
+  console.log("request body data", req.body.data);
+
   const allUsers = await prisma.employee.findMany();
   console.log(allUsers)
   res.json(allUsers);
@@ -103,67 +170,67 @@ app.post('/login', async (req, res)=>{
   res.json(datainfo);
 });
  */
-app.delete('/delete', async (req, res)=>{
-  console.log("request",req);
-  console.log("request body",req.body);
-  console.log("request body data",req.params);
-  
+app.delete('/delete', async (req, res) => {
+  console.log("request", req);
+  console.log("request body", req.body);
+  console.log("request body data", req.params);
+
   const deletePost = await prisma.employee.delete({
     where: {
       id: parseInt(req.query.id) // Random ID
     },
   })
 
-  
-  
+
+
 
   console.dir(deletePost);
-  
+
   res.json(deletePost);
 });
 
 
 
 
-app.put('/update/:id', async (req, res)=>{
+app.put('/update/:id', async (req, res) => {
 
-  
-  console.log("request",req);
-  console.log("request body",req.body);
-  console.log("request body data",req.params);
-  
+
+  console.log("request", req);
+  console.log("request body", req.body);
+  console.log("request body data", req.params);
+
   const deletePost = await prisma.employee.update({
     where: {
       id: parseInt(req.params.id) // Random ID
     },
     data: {
-        
-       name: req.body.name, addresss: req.body.address, order: req.body.order 
+
+      name: req.body.name, addresss: req.body.address, order: req.body.order
     },
   })
 
 
-console.dir(deletePost);
+  console.dir(deletePost);
 
-res.json(deletePost);
+  res.json(deletePost);
 });
 
 
 
-app.delete('/edit', async (req, res)=>{
-  console.log("edit request",req);
-  console.log("request body",req.body);
-  console.log("request body data",req.params);
-  
+app.delete('/edit', async (req, res) => {
+  console.log("edit request", req);
+  console.log("request body", req.body);
+  console.log("request body data", req.params);
+
   const editrecord = await prisma.employee.findUnique({
     where: {
       id: parseInt(req.query.id) // Random ID
     },
   })
-  
+
 
   console.log(editrecord);
-  
+
   res.json(editrecord);
 });
 
@@ -180,25 +247,25 @@ app.delete('/edit', async (req, res)=>{
 
 async function main() {
 
-/*   await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alicej@prisma.io',
-      posts: {
-        create: { title: 'Hello World' },
+  /*   await prisma.user.create({
+      data: {
+        name: 'Alice',
+        email: 'alicej@prisma.io',
+        posts: {
+          create: { title: 'Hello World' },
+        },
+        profile: {
+          create: { bio: 'I like turtles' },
+        },
       },
-      profile: {
-        create: { bio: 'I like turtles' },
+    }) */
+  /*   const allUsers = await prisma.user.findMany({
+      include: {
+        posts: true,
+        profile: true,
       },
-    },
-  }) */
-/*   const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  })
-  console.dir(allUsers, { depth: null }) */
+    })
+    console.dir(allUsers, { depth: null }) */
 }
 
 main()
@@ -208,8 +275,8 @@ main()
   .finally(async () => {
     await prisma.$disconnect()
   })
- 
-  app.listen(port, ()=>{
-    console.log(`Express is running at port ${port}`);
+
+app.listen(port, () => {
+  console.log(`Express is running at port ${port}`);
 });
 
